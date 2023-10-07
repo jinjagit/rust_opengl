55b6ea18cf38a2bb755e2bc132cde716744d2ef6
Simon Tharby <simontharby@gmail.com>
2023-10-07 14:41:11 +0100

    add glium crate and run opengl window

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Set up basic Rust framework for using OpenGL.
 
-Following [Understanding OpenGL basics in Rust](https://blog.logrocket.com/understanding-opengl-basics-rust/) guide, by Rosario De Chiara 
+Following [Understanding OpenGL basics in Rust](https://blog.logrocket.com/understanding-opengl-basics-rust/) guide, by Rosario De Chiara.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate glium;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = glium::glutin::event_loop::EventLoop::new();
+
+    let wb = glium::glutin::window::WindowBuilder::new()
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(800.0, 600.0))
+        .with_title("Hello world");
+
+    let cb = glium::glutin::ContextBuilder::new();
+
+    // You will get a warning on the next line but we will fix this later on
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+
+        match event {
+            glium::glutin::event::Event::WindowEvent { event, .. } => match event {
+                glium::glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glium::glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            _ => return,
+        }
+    });
 }
